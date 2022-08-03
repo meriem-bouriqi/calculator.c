47520f6a6a102a86a95517379c2d09ced8fbd97d
meriem-bouriqi <meriembouriqi@gmail.com>
2022-08-03 12:37:03 +0200

    modification

--- first-file.c
+++ first-file.c
@@ -9,8 +9,15 @@ printf("entez le nombre 1 :");
 scanf("%d", & NB1);
 printf("enterz l'operateur :");
 scanf("%s", &op);
+if (op == 'q')
+{printf("le programme arreter !!!");
+exit(0);
+}
+else
+{
 printf("entrez Nombre 2:");
 scanf("%d", & NB2);
+}
 switch (op)
 {
 case '+' : printf("%d", NB1+NB2 );
@@ -22,7 +29,7 @@ break;
 case '/': if (NB2!=0)
  printf("%d", NB1/NB2);
  else{
- printf("impossible"); 
+ printf("le division par 0 est impossible"); 
    }
 break;
 default: printf("ce calcul n'existe pas\n");
